3f326344761a6076f32f21bfd38008b9140c5869
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-09-10 18:29:09 -0300

    agrego app de recomendacion de colaboradores al proyecto

---
+++ recomendar_colaboradores_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecomendarColaboradoresAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recomendar_colaboradores_app'
