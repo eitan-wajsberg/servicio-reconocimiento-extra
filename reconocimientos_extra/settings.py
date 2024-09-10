b6e44a212862cabc2e8043efaafac46ccff2186a
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-09-10 18:01:31 -0300

    src: configuro la base de datos

--- reconocimientos_extra/settings.py
+++ reconocimientos_extra/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'reconocimientos_extra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'servicio-reconocimientos-extra',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
